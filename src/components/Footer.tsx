c12d7da659bdbc1b6ea6c776da0d78eb9b1f4a38
edmhok <121737342+edmhok@users.noreply.github.com>
2023-09-04 13:03:25 +0800

    Fix Footer Responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,20 @@ import FbIcon from '../../public/facebook.svg';
 import Twitter from '../../public/twitter.svg';
 import LinkeIn from '../../public/linkedin.svg';
 import { useState, useEffect } from 'react';
-import { Fab } from '@mui/material';
+import { Box, Fab, Grid, Paper, styled } from '@mui/material';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Link from 'next/link';
+
+const Item = styled(({ ...props }) => (
+  <div {...props} />
+))(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 
 const Footer = () => {
   const [hovered, setHovered] = useState(false);
@@ -33,86 +45,100 @@ const Footer = () => {
   }, []);
 
   return (
-    <div className='w-full h-[150px] pt-10'>
+    <Box sx={{ flexGrow: 1 }}>
 
       <div className="running-message-container w-full bg-black text-center text-white font-bold py-2 md:pb-2">
         <div className="running-message">Discover a new way to shop online with SmartSpend Ecommerce. We're bringing you a curated selection of the latest products, trends, and deals, all in one place.
-          Whether you're looking for fashion, electronics, home essentials, or more, we've got you covered.</div>
+          Whether you're looking for fashion, electronics, home essentials, or more, we've got you covered.
+        </div>
       </div>
 
-      <div className='grid grid-flow-col gap-x-[150px] justify-center pt-[50px] relative'>
-        <div className='absolute right-0 bottom-10'>
-          {isVisible &&
-            <div
-              className="text-gray p-2 cursor-pointer"
-              onClick={scrollToTop}
-            >
-
+      <div className='container'>
+        <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 2, sm: 8, md: 16 }}>
+          <Grid item xs={2} sm={4} md={3} textAlign={'center'}>
+            <div className='flex justify-center py-5'>
+              SmartSpend<br />
+              Philippines
             </div>
-          }
-          <Fab
-            onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}
-            color="secondary"
-            size="small"
+            <div className='flex flex-row justify-center space-x-2'>
+              <Image src={FbIcon} width={32} height={32} alt="icon" />
+              <Image src={Twitter} width={32} height={32} alt="icon" />
+              <Image src={LinkeIn} width={32} height={32} alt="icon" />
+            </div>
+          </Grid>
+          <Grid item xs={2} sm={4} md={3} textAlign={'center'}>
+            <div className="flex flex-col justify-center pt-5">
+              <h1 className='font-bold mb-[10px]'>Shop</h1>
+              <ul>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm mb-[10px]'>Categories</li></Link>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm mb-[10px]'>Best Seller</li></Link>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm mb-[20px]'>Promo</li></Link>
+              </ul>
+            </div>
+          </Grid>
+          <Grid item xs={2} sm={4} md={3} textAlign={'center'}>
+            <div className="flex flex-col justify-center pt-5">
+              <h1 className='font-bold mb-[10px]'>Dashboard</h1>
+              <ul>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm mb-[10px]'>Teams</li></Link>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm mb-[10px]'>Forum</li></Link>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm mb-[20px]'>Leaderboard</li></Link>
+              </ul>
+            </div>
+          </Grid>
+          <Grid item xs={2} sm={4} md={3} textAlign={'center'}>
+            <div className="flex flex-col justify-center pt-5">
+              <h1 className='font-bold mb-[10px]'>About</h1>
+              <ul>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm mb-[10px]'>Company</li></Link>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm mb-[10px]'>Careers</li></Link>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm mb-[10px]'>Blogs</li></Link>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm mb-[20px]'>News</li></Link>
+              </ul>
+            </div>
+          </Grid>
+          <Grid item xs={2} sm={4} md={3} textAlign={'center'}>
+            <div className="flex flex-col pt-5">
+              <h1 className='font-bold mb-[10px]'>Help</h1>
+              <ul>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm mb-[10px]'>FAQ</li></Link>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm mb-[10px]'>Privacy</li></Link>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm mb-[20px]'>Trust and Safety</li></Link>
+              </ul>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={6} >
+            <div className='flex justify-center pb-[20px]'>
+              <ul className='flex flex-row space-x-20'>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm'>Terms of Use</li></Link>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm'>License</li></Link>
+                <Link href='/' className='link' prefetch={false}><li className='text-sm'>Policy</li></Link>
+              </ul>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+      <div className='absolute right-0 bottom-10'>
+        {isVisible &&
+          <div
+            className="text-gray p-2 cursor-pointer"
             onClick={scrollToTop}
-            style={{ display: isVisible ? 'flex' : 'none' }}
           >
-            <KeyboardArrowUpIcon />
-          </Fab>
-        </div>
-        <div className='text-sm'>
-          <div className="mb-[28px] text-base font-bold">SmartSpend</div>
-          <div className="text-sm mb-[18px]">Davao City<br /> Philippines</div>
-          <div className="mb-[70px] flex direction-row space-x-2">
-            <Image src={FbIcon} width={32} height={32} alt="icon" />
-            <Image src={Twitter} width={32} height={32} alt="icon" />
-            <Image src={LinkeIn} width={32} height={32} alt="icon" />
+
           </div>
-        </div>
-
-        <div><h1 className='font-bold mb-[28px]'>Shop</h1>
-
-          <ul>
-            <li className='text-sm mb-[18px]'>Categories</li>
-            <li className='text-sm mb-[18px]'>Best Seller</li>
-            <li className='text-sm mb-[18px]'>Promo</li>
-          </ul>
-        </div>
-
-        <div><h1 className='font-bold mb-[28px]'>Dashboard</h1>
-          <ul>
-            <li className='text-sm mb-[18px]'>Teams</li>
-            <li className='text-sm mb-[18px]'>Forum</li>
-            <li className='text-sm mb-[18px]'>Leaderboard</li>
-          </ul>
-        </div>
-        <div><h1 className='font-bold mb-[28px]'>About</h1>
-          <ul>
-            <li className='text-sm mb-[18px]'>Company</li>
-            <li className='text-sm mb-[18px]'>Careers</li>
-            <li className='text-sm mb-[18px]'>Blogs</li>
-            <li className='text-sm mb-[18px]'>News</li>
-          </ul>
-        </div>
-        <div><h1 className='font-bold mb-[28px]'>Help</h1>
-          <ul>
-            <li className='text-sm mb-[18px]'>FAQ</li>
-            <li className='text-sm mb-[18px]'>Privacy</li>
-            <li className='text-sm mb-[18px]'>Trust and Safety</li>
-          </ul>
-        </div>
+        }
+        <Fab
+          onMouseEnter={() => setHovered(true)}
+          onMouseLeave={() => setHovered(false)}
+          color="success"
+          size="small"
+          onClick={scrollToTop}
+          style={{ display: isVisible ? 'flex' : 'none' }}
+        >
+          <KeyboardArrowUpIcon />
+        </Fab>
       </div>
-      <div className='container pb-[50px] pl-[180px]'>
-        <ul className='flex direction-row space-x-20'>
-          <li className='text-sm'>© 2023 SmartSpend Company</li>
-          <li className='text-sm'>Terms of Use</li>
-          <li className='text-sm'>License</li>
-          <li className='text-sm'>Policy</li>
-        </ul>
-
-      </div>
-    </div>
+    </Box>
   )
 }
 
