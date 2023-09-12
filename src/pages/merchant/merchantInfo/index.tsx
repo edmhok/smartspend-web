c5b27a23ebb5182c1b3b9fd34e06be11efecc339
Elchen Tanauan <lchentanauan@gmail.com>
2023-09-12 17:42:35 +0800

    WIP MerchanInfo

---
+++ src/pages/merchant/merchantInfo/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import type { NextPage } from "next";
+import MyInfo from "@/components/myinfo/MerchantInfo";
+
+const MerchantInfoId: NextPage = () => (
+  <>
+    <MyInfo />
+  </>
+);
+
+export default MerchantInfoId;
